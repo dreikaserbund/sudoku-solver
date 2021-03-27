4d475270d48e2f6e19cd04ffbd16c40b689d5eb2
parak <parak@localhost.localdomain>
2021-03-27 17:25:13 -0400

    Add object variables to Cell.__init__
    Add self.digit, self.coordinates, self.possibilities, and x and y args to __init__

--- python-solver/solver.py
+++ python-solver/solver.py
@@ -2,5 +2,7 @@
 
 class Cell:
 	"""One cell on a grid"""
-	def __init__(self):
-		pass
+	def __init__(self, x, y):
+		self.digit = None
+		self.coordinates = (x, y)
+		self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
