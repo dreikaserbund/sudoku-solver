79585842953083dedfcf956912bc88790bb69070
parak <parak@localhost.localdomain>
2021-04-29 13:42:15 -0400

    Add debug_print_board function

--- python-solver/solver.py
+++ python-solver/solver.py
@@ -1,3 +1,11 @@
 """Solves sudoku puzzles"""
 
+import time
 from grid.py import Grid, Cell
+
+
+def debug_print_board(grid, debug):
+	"""Prints grid and sleeps .5sec if debug True"""
+	if debug:
+		print(grid)
+		time.sleep(.5)
