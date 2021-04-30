95940ae6acd39bd55534b2510af19500f5486579
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2021-04-30 15:56:15 -0400

    Add basic solving method
    Solves puzzles with simple algorithm. Doesn't work for harder puzzles

--- python-solver/solver.py
+++ python-solver/solver.py
@@ -10,3 +10,21 @@ def debug_print_board(grid, debug):
 		print(grid)
 		print("\n\n")
 		time.sleep(.5)
+
+def solve(hints, debug=False):
+	grid = Grid(hints)
+	print("Unsolved:")
+	print(grid)
+	print("\n\n")
+	while len(grid.solved_cells) < 81:
+		for solved in grid.solved_cells:
+			for unsolved in filter(lambda x: not x.digit, solved.neighbors):
+				if solved.digit in unsolved.possibilities:
+					unsolved.remove_possibility(solved.digit)
+				if len(unsolved.possibilities) == 1:
+					unsolved.set_digit(unsolved.possibilities[0])
+					debug_print_board(grid, debug)
+	print("Solved:")
+	print(grid)
+	print("\n\n")
+
