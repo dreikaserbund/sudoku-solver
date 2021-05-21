b28ffdd09f3e546f4bd6e23cdb41e04b1b53bdb4
leo-at-crefeld <47148174+leo-at-crefeld@users.noreply.github.com>
2021-05-21 12:53:22 -0400

    Add ability to track if solver is stuck or not
    Adds board_unchanged and stuck booleans which track if the board has been changed in one cycle of the main while loop.
    
    This will allow the solver to use alternative methods to try and solve the board if the initial method doesn't work.

--- python-solver/solver.py
+++ python-solver/solver.py
@@ -14,13 +14,17 @@ def debug_print_board(grid, debug):
 
 def solve(grid, debug=False):
 	while len(grid.solved_cells) < 81:
+		board_unchanged = False
 		for solved in grid.solved_cells:
 			for unsolved in filter(lambda x: not x.digit, solved.neighbors):
 				if solved.digit in unsolved.possibilities:
 					unsolved.remove_possibility(solved.digit)
+					board_unchanged = True
 				if len(unsolved.possibilities) == 1:
 					unsolved.set_digit(unsolved.possibilities[0])
+					board_unchanged = True
 					debug_print_board(grid, debug)
+		stuck = not board_unchanged
 	print("Solved:")
 	print(grid)
 	print("\n\n")
